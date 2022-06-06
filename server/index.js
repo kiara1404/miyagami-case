4618d8cfc0447ea524b9aacee506e804f269c6e8
Kiara <kiarabroekhuizen1@hotmail.com>
2022-06-06 17:03:13 +0200

    finally connected API to frontend

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import Flickr from 'flickr-sdk';
+import 'dotenv';
+import axios from 'axios';
+
+const flickr = new Flickr('c274e2f7d9f965e5f625fb9aa45d02a8')
+const app = express();
+const port = process.env.PORT || 5000
+
+
+
+app.get('/api', async (req, res) => {
+
+    try {
+        const response = await flickr.photos.search({
+            text: "flower",
+            per_page: 16
+        });
+        res.json(response.body)
+        console.log(response.body.photos.photo)
+    } catch (err) {
+        console.log(err);
+    }
+});
+// server
+app.listen(port, () =>
+    console.log(`Server is running succesfully👋!`),
+);
