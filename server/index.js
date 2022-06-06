b04a593a7b3e937b4e6561d7b7194dd11c94c91b
Kiara <kiarabroekhuizen1@hotmail.com>
2022-06-06 17:04:26 +0200

    cleanup package.json

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import Flickr from 'flickr-sdk';
 import 'dotenv';
-import axios from 'axios';
+
 
 const flickr = new Flickr('c274e2f7d9f965e5f625fb9aa45d02a8')
 const app = express();
